db1ef55ed0922c85b5d8b9a7c4bf6a92d385e872
Kopperun Silambazhagii <silambazhagii@Silambazhagiis-MacBook-Pro-M1.local>
2025-05-01 16:07:46 +0530

    updated

--- core/admin.py
+++ core/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
 from .models import Employee, TrainingProgram, Enrollment
 
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'department')
-    search_fields = ('name', 'email', 'department')
-
-@admin.register(TrainingProgram)
-class TrainingProgramAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'start_date', 'end_date')
-    search_fields = ('title',)
-
-@admin.register(Enrollment)
-class EnrollmentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'employee', 'program', 'enrolled_on')
-    search_fields = ('employee__name', 'program__title')
+admin.site.register(Employee)
+admin.site.register(TrainingProgram)
+admin.site.register(Enrollment)
--- core/views.py
+++ core/views.py
@@ -1,31 +1,6 @@
-from django.shortcuts import render, redirect
-from .models import Employee, TrainingProgram
-from .forms import EmployeeForm, TrainingProgramForm
+from django.shortcuts import render
+from .models import Employee
 
 def employee_list(request):
-    employees = Employee.objects.all()
+    employees = Employee.objects.all()  # Fetch all employees
     return render(request, 'core/employee_list.html', {'employees': employees})
-
-def employee_add(request):
-    if request.method == 'POST':
-        form = EmployeeForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('employee_list')
-    else:
-        form = EmployeeForm()
-    return render(request, 'core/employee_add.html', {'form': form})
-
-def training_list(request):
-    trainings = TrainingProgram.objects.all()
-    return render(request, 'core/training_list.html', {'trainings': trainings})
-
-def training_add(request):
-    if request.method == 'POST':
-        form = TrainingProgramForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('training_list')
-    else:
-        form = TrainingProgramForm()
-    return render(request, 'core/training_add.html', {'form': form})
